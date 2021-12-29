57f1d3b76b87353ae8f8c2a6bf12acf4be915f78
Gustavo <gar.santeliz@gmail.com>
2021-12-28 19:13:51 -0500

    creating different function to fetch data and render it

--- src/News/News.js
+++ src/News/News.js
@@ -9,10 +9,24 @@ export const News = () =>{
              news: [],
          };
      }
+     function FetchMethod(){
+        const url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=db6cc17ae2084784bd54294f72ee04f6';
+
+        fetch(url)
+        .then((response)=>{
+            return response.json();
+        })
+        .then((data)=>{
+            this.setState({
+                news: data.articles
+            })
+        })
+        .catch((error)=> console.log(error));
+     };
 
     function renderItems (){
         return (
-            items.map((item)=>{
+            state.news.map((item)=>{
                 <NewSingle key={item.id} item={item} />
             })
         )
