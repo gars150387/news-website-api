2e007588f98e17b54ffd54a3cf130a7382b2a4f3
Gustavo <gar.santeliz@gmail.com>
2021-12-28 19:25:02 -0500

    defining variable

--- src/News/News.js
+++ src/News/News.js
@@ -1,39 +1,40 @@
 import React from 'react'
-import NewSingle from './NewSingle'
+import { NewSingle } from './NewSingle'
 
-export const News = () =>{
+export const News = () => {
 
-     function Constructor (props){
-         super(props);
-         this.state= {
-             news: [],
-         };
-     }
-     function FetchMethod(){
+    function Constructor(props) {
+        //  super(props);
+        this.state = {
+            news: [],
+        };
+    };
+
+    function FetchMethod() {
         const url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=db6cc17ae2084784bd54294f72ee04f6';
 
         fetch(url)
-        .then((response)=>{
-            return response.json();
-        })
-        .then((data)=>{
-            this.setState({
-                news: data.articles
+            .then((response) => {
+                return response.json();
             })
-        })
-        .catch((error)=> console.log(error));
-     };
+            .then((data) => {
+                this.setState({
+                    news: data.articles
+                })
+            })
+            .catch((error) => console.log(error));
+    };
 
-    function renderItems (){
-        return (
-            state.news.map((item)=>{
-                <NewSingle key={item.id} item={item} />
-            })
-        )
-    }
+    <div>
+        {News.map((item) => {
+            <NewSingle key={item.id} item={item} />
+        })}
+    </div>
+
     return (
         <ul>
-            {renderItems()}
+            {News()}
         </ul>
     )
+    
 }
