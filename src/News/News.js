37de9a3311fb3ffcc73478b04b7ab3c803dd6c98
Gustavo <gar.santeliz@gmail.com>
2021-12-28 19:06:29 -0500

    structuring code

--- src/News/News.js
+++ src/News/News.js
@@ -2,6 +2,14 @@ import React from 'react'
 import NewSingle from './NewSingle'
 
 export const News = () =>{
+
+     function Constructor (props){
+         super(props);
+         this.state= {
+             news: [],
+         };
+     }
+
     function renderItems (){
         return (
             items.map((item)=>{
