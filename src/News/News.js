9d70e465c1bc5f3ed4f829b44069b6e734802335
Gustavo <gar.santeliz@gmail.com>
2022-01-02 18:27:01 -0500

    trying with old version synta

--- src/News/News.js
+++ src/News/News.js
@@ -1,49 +1,28 @@
-import React from 'react'
-import { NewSingle } from './NewSingle'
+import { render } from '@testing-library/react';
+import React, {Component} from 'react'
+// import { NewSingle } from './NewSingle'
 
-export const News = () => {
-
-function State(){
-    let news = []
-    return(
-        State={
-            news: news
+class News extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            news: []
         }
-    )
-}
+    }
+};
 
-    function FetchMethod() {
-        let news = []
-        const url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=db6cc17ae2084784bd54294f72ee04f6';
+function renderItems() {
+    return this.props.items.map((item)=>{
+        <NewSingle key={item.id} item={item} />
+    });
+};
 
-        fetch(url)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                this.setState({
-                    news: data.articles,
-                    
-                })
-                console.log(news)
-            })
-            .catch((error) => console.log(error));
-            {news.map((item) => {
-                <NewSingle key={item.id} item={item} className="col s8" />
-            })}
-    
-    };
-
-    // <div>
-    //     {news.map((item) => {
-    //         <NewSingle key={item.id} item={item} />
-    //     })}
-    // </div>
-
+function render(){
     return (
         <ul>
-            {News()}
+            {this.renderItems()}
         </ul>
     )
-    
 }
+
+export default News;
