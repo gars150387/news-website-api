ce36288ceb957abf995e8722660b5d5960bae0be
Gustavo <gar.santeliz@gmail.com>
2022-01-03 18:06:17 -0500

    re defining component

--- src/News/News.js
+++ src/News/News.js
@@ -1,28 +1,30 @@
-import { render } from '@testing-library/react';
 import React, {Component} from 'react'
-// import { NewSingle } from './NewSingle'
+import { NewSingle } from './NewSingle'
 
-class News extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            news: []
+
+export const News = () =>{
+    class News extends Component{
+        constructor(props){
+            super(props);
+            this.state = {
+                news: []
+            }
         }
+    };
+    
+    function renderItems() {
+        return this.props.items.map((item)=>{
+            <NewSingle key={item.id} item={item} />
+        });
+    };
+    
+    function render(){
+        return (
+            <ul>
+                {this.renderItems()}
+            </ul>
+        )
     }
-};
-
-function renderItems() {
-    return this.props.items.map((item)=>{
-        <NewSingle key={item.id} item={item} />
-    });
-};
-
-function render(){
-    return (
-        <ul>
-            {this.renderItems()}
-        </ul>
-    )
+    
 }
 
-export default News;
