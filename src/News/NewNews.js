80249ee2d115d09d00123c74d9528b8e094a7881
Gustavo <gar.santeliz@gmail.com>
2022-01-04 19:46:53 -0500

    using useEffect to fetching data

--- src/News/NewNews.js
+++ src/News/NewNews.js
@@ -1,35 +1,27 @@
-import React from 'react'
-import {NewSingle} from './NewSingle'
+import React, { useState, useEffect } from 'react'
+// import { NewSingle } from './NewSingle'
 
-export const NewNews = ()=>{
+export const NewNews = () => {
 
-    let state = {
-        news: []
-    };
+    const [data, setData] = useState([]);
 
-    const url = 'https://newsapi.org/v2/everything?q=tesla&from=2021-12-04&sortBy=publishedAt&apiKey=db6cc17ae2084784bd54294f72ee04f6';
-
-    fetch(url)
-    .then((response)=>{
-        return response.json();
-    })
-  .then ((data)=>{
-      JSON.stringify(data)
-      console.log('data', data)
-      state.news.push(data)
-      console.log('news', state.news)
-  })
-    .catch((error)=> console.log(error))
-
-    return (
+    useEffect(() => {
+        fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-12-05&sortBy=publishedAt&apiKey=db6cc17ae2084784bd54294f72ee04f6')
+          .then((response) => {
+            return response.json();
+          })
+          .then(setData => {
+            JSON.stringify(setData)
+            console.log('data', setData)
+          })
+      }, []);
+      return (
         <>
-            {state.news.articles.map((item)=>{
-                return(
-                    <li>
-                        <NewSingle key={item.title} />
-                    </li>
-                )
-            })}
+          <ul>
+            {data.map((articles) => (
+              <li key={articles.id}>{articles.title}</li>
+            ))}
+          </ul>
         </>
-    )
+      )
 }
