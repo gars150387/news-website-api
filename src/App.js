fc6fde379e51ab30661410401ea0d908d6fee30a
Gustavo <gar.santeliz@gmail.com>
2022-01-06 19:29:00 -0500

    removing code and imported component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './App.css';
+import { NewNews } from './News/NewNews';
 
-function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-12-05&sortBy=publishedAt&apiKey=db6cc17ae2084784bd54294f72ee04f6')
-      .then((response) => {
-        return response.json();
-      })
-      .then(setData => {
-        JSON.stringify(setData)
-        console.log('data', setData)
-        Object.values(setData.articles)
-        console.log('data2', setData)
-      })
-  }, []);
+export const App =()=> {
   return (
     <>
-      <ul>
-        {data.map((article, index) => (
-          <li key={article.id} article={article} index={index}>{article.title}</li>
-        ))}
-      </ul>
+      <NewNews />
     </>
   )
-
-
 }
-
-export default App;
