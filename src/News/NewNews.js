d51152779559bff3239e63d2dcfa1d1fc2a5321d
Gustavo <gar.santeliz@gmail.com>
2022-01-06 19:34:49 -0500

    trying different ways to fetch data

--- src/News/NewNews.js
+++ src/News/NewNews.js
@@ -3,10 +3,10 @@ import { NewSingle } from './NewSingle';
 
 export const NewNews = () => {
 
-    const [data, setData] = useState([]);
+    const [data] = useState([]);
 
     useEffect(() => {
-        fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-12-05&sortBy=publishedAt&apiKey=db6cc17ae2084784bd54294f72ee04f6')
+        fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-12-10&sortBy=publishedAt&apiKey=db6cc17ae2084784bd54294f72ee04f6')
           .then((response) => {
             return response.json();
           })
