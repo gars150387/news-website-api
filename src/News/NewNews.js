243d2e7a3a7524cda410294cbb0a282683be684e
Gustavo <gar.santeliz@gmail.com>
2022-01-28 19:20:15 -0500

    structuring to render data

--- src/News/NewNews.js
+++ src/News/NewNews.js
@@ -3,24 +3,23 @@ import { NewSingle } from './NewSingle';
 
 export const NewNews = () => {
 
-    const [data, setData] = useState([]);
+    const [setData] = useState([]);
 
     useEffect(() => {
-        fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-12-15&sortBy=publishedAt&apiKey=db6cc17ae2084784bd54294f72ee04f6')
+        fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-01-01&sortBy=publishedAt&apiKey=db6cc17ae2084784bd54294f72ee04f6')
           .then((response) => {
             return response.json();
           })
           .then(setData => {
             JSON.stringify(setData)
-            console.log('data', setData)
-           
+            console.log('data', setData)           
           })
           .catch((error)=> console.log(error))
       }, []);
       return (
         <>
           <ul>
-            {data.articles && data.articles.map((article, index) => (
+            {setData.articles && setData.articles.map((article, index) => (
               <NewSingle article={article} index={index} />
             ))}
           </ul>
