8f6d631ddf6f011a69e0fc492d72e8f2e1fec7e4
Gustavo <gar.santeliz@gmail.com>
2022-02-18 19:41:04 -0500

    deleted component where the data would pass to render element in array

--- src/News/NewNews.js
+++ src/News/NewNews.js
@@ -3,26 +3,37 @@ import { NewSingle } from './NewSingle';
 
 export const NewNews = () => {
 
-    const [setData] = useState([]);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-01-01&sortBy=publishedAt&apiKey=db6cc17ae2084784bd54294f72ee04f6')
+        fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-&sortBy=publishedAt&apiKey=db6cc17ae2084784bd54294f72ee04f6')
           .then((response) => {
             return response.json();
           })
-          .then(setData => {
-            JSON.stringify(setData)
-            console.log('data', setData)           
+          .then(data => {
+            JSON.stringify(data)
+            console.log('data', data)  
+            // console.log('set' , setData)        
           })
           .catch((error)=> console.log(error))
       }, []);
+
+
+      
       return (
         <>
-          <ul>
-            {setData.articles && setData.articles.map((article, index) => (
-              <NewSingle article={article} index={index} />
-            ))}
-          </ul>
+            {data.articles && data.articles.map((article) =>{
+              return (
+                <li key={article.id}>
+                <img src={article.urlToImage}  alt={article.title} />
+                <p>{article.title}</p>
+                <p>{article.author}</p>
+                <p>{article.description}</p>
+                <p>{article.publishdAt}</p>
+                <button href={article.url}>Read full article</button>
+            </li>
+              )
+            })}
         </>
       )
 }
