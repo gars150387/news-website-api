c388deaae08ee46fe1d4ffb7ba5af9b38c56cf7e
Gustavo <gar.santeliz@gmail.com>
2022-02-18 19:41:29 -0500

    working with news api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
+// import { DogAPI } from './components/DogAPI';
 import { NewNews } from './News/NewNews';
 
 export const App =()=> {
   return (
     <>
       <NewNews />
+      {/* <DogAPI /> */}
     </>
   )
 }
